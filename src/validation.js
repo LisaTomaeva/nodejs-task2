335a49c3c4285e817e5c34c2930b50ea0b05957f
Elizaveta Tomaeva <elizaveta_tomaeva@epam.com>
2021-04-12 18:00:29 +0300

    validation fixes

--- src/validation.js
+++ src/validation.js
@@ -1,7 +1,7 @@
 const Joi = require('joi')
  
 export const bodySchema = Joi.object({
-  login: Joi.string().required(),
+  login: Joi.string().pattern(new RegExp('[A-Za-z]')).required(),
   age: Joi.number().min(4).max(130).required(),
   password: Joi.string().pattern(new RegExp('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$')).required(),
 })
